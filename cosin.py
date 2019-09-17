5897cbe2ee1ea4f7753cf8b22ab5cbacba937c65
SYH <c95syh@ncsist.org.tw>
2019-09-17 16:21:14 +0800

    del Hello add cos

---
+++ cosin.py
@@ -0,0 +1,13 @@
+def cosin_distance(vector1, vector2):
+    dot_product = 0.0
+    normA = 0.0
+    normB = 0.0
+    for a, b in zip(vector1, vector2):
+        dot_product += a * b
+        normA += a ** 2
+        normB += b ** 2
+    if normA == 0.0 or normB == 0.0:
+        return None
+    else:
+        return dot_product / ((normA * normB) ** 0.5)
+
