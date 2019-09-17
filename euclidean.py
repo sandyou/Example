149099772a6db2932e90b8ea452b8340ce998fbd
SYH <c95syh@ncsist.org.tw>
2019-09-17 16:15:03 +0800

    update Readme add euclidean

---
+++ euclidean.py
@@ -0,0 +1,6 @@
+
+def get_distance(data1, data2):
+    points = zip(data1, data2)
+    diffs_squared_distance = [pow(a - b, 2) for (a, b) in points]
+    return math.sqrt(sum(diffs_squared_distance))
+
