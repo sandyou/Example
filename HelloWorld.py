cf53e35c9b08577ab8a384e3eba7d1fe66dc17f6
SYH <c95syh@ncsist.org.tw>
2019-09-17 15:40:16 +0800

    edit Readme add helloworld code

---
+++ HelloWorld.py
@@ -0,0 +1,14 @@
+X!上班好累！
+# -*- coding: UTF-8 -*-
+
+import datetime
+
+x = datetime.datetime.now()  # 現在時間
+
+h = 16 - x.hour
+m = 60 - x.minute
+
+if (x.minute == 60):
+    h = h + 1
+
+print ('還有 %s時 %s分就可以下班啦！！'%(h, m)) 
